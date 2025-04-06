fc466347c3aad7bdefa9fbb59965a60f01ea0cf8
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-06 13:11:40 -0500

    make StateManager class

--- systems/state_manager.js
+++ systems/state_manager.js
@@ -1,14 +1,157 @@
-import storageObject from "https://esm.sh/gh/jeff-hykin/storage-object@0.0.3.5/main.js"
-import { dump, load } from "https://esm.sh/js-yaml@4.1.0/"
+import storageObject from "../imports/storage_object.js"
+import { dump, load } from "../imports/js_yaml.js"
 // import debounce from "https://esm.sh/lodash@4.17.21/debounce.js"
 import throttle from "https://esm.sh/lodash@4.17.21/throttle.js"
 const yaml = { stringify: dump, parse: load }
 
 const localStorageKey = "activeState"
+// TODO
+    // save to local storage
+    // throttle(()=>{
+    //     storageObject[localStorageKey] = this.toYaml()
+    // }, 1000)
+
+export class StateManager {
+    constructor({ initalData, loadStateCallbacks, stateChangeCallbacks, }={}) {
+        this.cells = initalData?.cells||[]
+        this.activeState = structuredClone({
+            ...initalData,
+            cells: initalData?.cells||[],
+            // DEBUGGING override
+            cells: [
+                {
+                    cellId: Math.random(),
+                    type: "file",
+                    filePath: "test.js",
+                    coreContent: "howdy howdy",
+                    varName: "test",
+                },
+                {
+                    cellId: Math.random(),
+                    type: "jsCode",
+                    coreContent: "console.log('howdy')\n\n\n\n",
+                },
+                {
+                    cellId: Math.random(),
+                    type: "markdown",
+                    coreContent: "console.log('howdy')\n\n\n\n",
+                },
+            ]
+        })
+        this.prevState = structuredClone(activeState)
+        this.loadStateCallbacks = new Set([...loadStateCallbacks])
+        this.stateChangeCallbacks = new Set([...stateChangeCallbacks])
+    }
+
+    toYaml() {
+        return yaml.stringify(this.activeState)
+    }
+    
+    // listener
+    onLoadState(callback) {
+        this.loadStateCallbacks.add(callback)
+        return this.loadStateCallbacks.size-1
+    }
+
+    // trigger
+    loadDataFromYaml(yamlString) {
+        let obj = yaml.parse(yamlString)
+        if (!(obj?.cells instanceof Array)) {
+            obj = {
+                cells: [],
+            }
+        }
+        for (const [key, value] of Object.entries(obj)) {
+            delete this.activeState[key]
+        }
+        Object.assign(this.activeState, obj)
+        this.prevState = structuredClone(this.activeState)
+        let returnVals = []
+        for (let each of new Set([...this.loadStateCallbacks, ...this.stateChangeCallbacks])) {
+            try {
+                returnVals.push(each(this.activeState))
+            } catch (error) {
+                console.error(error?.stack||error)
+            }
+        }
+        return returnVals
+    }
+    
+    // listener
+    onStateChange(callback) {
+        this.stateChangeCallbacks.add(callback)
+        return this.stateChangeCallbacks.size-1
+    }
+    // trigger
+    _activeStateWasUpdated() {
+        // ensure the new state is yaml-able
+        try {
+            this.toYaml() // attempt trigger error 
+            this.prevState = structuredClone(this.activeState)
+        } catch (error) {
+            const stateClone = structuredClone(this.prevState)
+            for (const [key, value] of Object.entries(this.activeState)) {
+                delete this.activeState[key]
+            }
+            Object.assign(this.activeState, stateClone)
+            throw error
+        }
+        let returnVals = []
+        for (let each of this.stateChangeCallbacks) {
+            try {
+                returnVals.push(each(this.activeState))
+            } catch (error) {
+                console.error(error?.stack||error)
+            }
+        }
+        return returnVals
+    }
+    getCellFromId(cellId) {
+        for (const each of this.activeState.cells) {
+            if (each.cellId == cellId) {
+                return each
+            }
+        }
+        return {}
+    }
+    injectCellAfter({existingCell, cellToInject}) {
+        const cellId = existingCell.cellId
+        let index = -1
+        for (const each of this.activeState.cells) {
+            index++
+            if (each.cellId == cellId) {
+                this.activeState.cells.splice(index+1, 0, cellToInject)
+                break
+            }
+        }
+        this._activeStateWasUpdated()
+    }
+    injectCellBefore({existingCell, cellToInject}) {
+        const cellId = existingCell.cellId
+        let index = -1
+        for (const each of this.activeState.cells) {
+            index++
+            if (each.cellId == cellId) {
+                this.activeState.cells.splice(index, 0, cellToInject)
+                break
+            }
+        }
+        this._activeStateWasUpdated()
+    }
+    removeCellData({cellId}) {
+        let index = -1
+        for (const each of this.activeState.cells) {
+            index++
+            if (each.cellId == cellId) {
+                this.activeState.cells.splice(index, 1)
+                break
+            }
+        }
+        this._activeStateWasUpdated()
+    }
+}
 
 export const activeState = {
-    settings: {
-    },
     cells: [],
     // [
     //     {
@@ -91,10 +234,8 @@ let prevState = activeState
     // trigger(s)
     export const loadDataFromYaml = (yamlString)=>{
         let obj = yaml.parse(yamlString)
-        if (!(obj?.cells instanceof Array) || !(obj?.settings instanceof Object)) {
+        if (!(obj?.cells instanceof Array)) {
             obj = {
-                settings: {
-                },
                 cells: [
                     {
                         cellId: Math.random(),
