3a364ff9d55157128b2dfcecddf267e1a4f5863b
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-09 11:07:49 -0500

    file system stub

--- systems/state_manager.js
+++ systems/state_manager.js
@@ -163,6 +163,9 @@ export class StateManager {
         )
         this._theme = merge({ oldData: this._theme, newData: value})
     }
+
+    get fileSystemData() { return Object.freeze(structuredClone(this._fileSystemData))  }
+    // set fileSystemData(value) {
     
     // 
     // serializing
