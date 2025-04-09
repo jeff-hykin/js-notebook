9eeceec28ce87877d5221a27a97e4bf1aec9b31d
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-09 12:46:01 -0500

    add document shim

--- tools/js_runtime.js
+++ tools/js_runtime.js
@@ -1,6 +1,7 @@
 import { convertImports } from "./parsing.js"
 import { isValidIdentifier } from "../imports/good_js.js"
 import { toRepresentation } from "../imports/good_js.js"
+import { allKeyDescriptions } from 'https://esm.sh/gh/jeff-hykin/good-js@1.15.0.0/source/flattened/all_key_descriptions.js'
 
 const { console, Math, Date, setTimout, setInterval, clearTimeout, clearInterval, fetch, Uint8Array, Map, Set, URL, WebAssembly, Array, Number, Symbol, Promise, RegExp, Error, document } = globalThis
 const elementLogger = (element, tag, ...args)=>{
@@ -59,9 +60,133 @@ const consoleElement = {
         }
     },
 }
+// enable remapping of document based on the caller (each runtime can get a different document)
+const enableDocumentShimWarnings = true
+let documentShimMapping = {}
+if (typeof document != "undefined") {
+    documentShimMapping = globalThis[Symbol.for('documentShimMapping')]
+    if (!documentShimMapping) {
+        documentShimMapping = globalThis[Symbol.for('documentShimMapping')] = {
+            // [cellId]: {
+            //     checker: (stack)=>true, stack is an array of strings
+            //     document: document,
+            // },
+            // ex:
+            // "19347": {
+            //     checker: (stack)=>stack.at(-1).includes("@debugger eval"),
+            //     document: {
+            //         DOCUMENT_NODE: "it works!"
+            //     },
+            // }
+        }
+        // shim all the methods/keys etc
+        for (let [key, {value, get, set, enumerable, configurable, writable}] of Object.entries(allKeyDescriptions(document))) {
+            if (typeof get == "function"||typeof set == "function") {
+                if (get) {
+                    const realGet = get
+                    get = (...args)=>{
+                        const stack = new Error().stack.split(/\n\s*/g).slice(2).map(each=>each.replace(/^at /,"")).filter(each=>each.length)
+                        for (const [cellId, {checker,document}] of Object.entries(documentShimMapping)) {
+                            if (typeof checker == "function" && checker(stack)) {
+                                return realGet.apply(document, args)
+                            }
+                        }
+                        return realGet.apply(document, args)
+                    }
+                }
+                if (set) {
+                    const realSet = set
+                    set = (...args)=>{
+                        const stack = new Error().stack.split(/\n\s*/g).slice(2).map(each=>each.replace(/^at /,"")).filter(each=>each.length)
+                        for (const [cellId, {checker,document}] of Object.entries(documentShimMapping)) {
+                            if (typeof checker == "function" && checker(stack)) {
+                                return realSet.apply(document, args)
+                            }
+                        }
+                        return realSet.apply(document, args)
+                    }
+                }
+                try {
+                    Object.defineProperty(document, key, {
+                        get,
+                        set,
+                        enumerable,
+                        configurable,
+                    })
+                } catch (error) {
+                    enableDocumentShimWarnings && console.warn(`Could not shim document.${key}`, error)
+                }
+            // value
+            } else {
+                if (typeof value == "function") {
+                    const realMethod = value
+                    value = (...args)=>{
+                        const stack = new Error().stack.split(/\n\s*/g).slice(2).map(each=>each.replace(/^at /,"")).filter(each=>each.length)
+                        for (const [cellId, {checker,document}] of Object.entries(documentShimMapping)) {
+                            if (typeof checker == "function" && checker(stack)) {
+                                return realMethod.apply(document, args)
+                            }
+                        }
+                        return realMethod.apply(document, args)
+                    }
+                    // make even the function look like the real one
+                    Object.defineProperty(value, "name", {
+                        value: realMethod.name,
+                    })
+                    value.toString = realMethod.toString.bind(realMethod)
+                    try {
+                        Object.defineProperty(document, key, {
+                            value,
+                            enumerable,
+                            configurable,
+                            writable,
+                        })
+                    } catch (error) {
+                        enableDocumentShimWarnings && console.warn(`Could not shim document.${key}`, error)
+                    }
+                } else {
+                    try {
+                        let set
+                        if (writable) {
+                            set = (newValue)=>{
+                                const stack = new Error().stack.split(/\n\s*/g).slice(2).map(each=>each.replace(/^at /,"")).filter(each=>each.length)
+                                for (const [cellId, {checker,document}] of Object.entries(documentShimMapping)) {
+                                    if (typeof checker == "function" && checker(stack)) {
+                                        return document[key] = newValue
+                                    }
+                                }
+                                value = newValue
+                                return newValue
+                            }
+                        }
+                        Object.defineProperty(document, key, {
+                            get: ()=>{
+                                const stack = new Error().stack.split(/\n\s*/g).slice(2).map(each=>each.replace(/^at /,"")).filter(each=>each.length)
+                                for (const [cellId, {checker,document}] of Object.entries(documentShimMapping)) {
+                                    if (typeof checker == "function" && checker(stack)) {
+                                        return document[key]
+                                    }
+                                }
+                                return value
+                            },
+                            set,
+                            enumerable,
+                            configurable,
+                            // writable,
+                        })
+                    } catch (error) {
+                        enableDocumentShimWarnings && console.warn(`Could not shim document.${key}`, error)
+                    }
+                }
+            }
+        }
+    }
+}
 const builtins = {
+    document,
     "console": consoleElement,
     "Math": {
+        ...Math,
         random: ()=>Math.random(),
     },
     "require": (path)=>{
@@ -95,13 +220,14 @@ export const makeRuntime = ({ randomSeed, }={}) => {
  * console.log("final runtime is:", runtime)
  * ```
  */
-export const runCode = async ({ code, runtime, outputElement }) => {
+export const runCode = async ({ code, runtime, outputElement, document }) => {
     code = convertImports(code)
     // global overrides
     runtime.globalThis = runtime.globalThis || runtime
-    runtime.$out = outputElement
+    runtime.$out = outputElement // TODO: remove this before release
     runtime.console = { ...consoleElement, ...runtime.console }
     runtime.console.$element = outputElement
+    runtime.document = document || runtime.document
 
     const variableNames = [
         ...new Set(
@@ -115,7 +241,7 @@ export const runCode = async ({ code, runtime, outputElement }) => {
     // console.debug(`code is:`,newCode)
     let cellAsFunction
     try {
-        // run a non-local eval, so there are no variable leaks
+        // run a non-local eval, so there are no variables (from this function) leaking into the runtime
         cellAsFunction = eval?.(newCode)
     } catch (error) {
         // basically only syntax errors are possible here
