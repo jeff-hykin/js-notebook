98393f1c2fa164f8ec5c25fb07308a491176c4aa
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-11 14:35:24 -0500

    add alpha version of file system

--- systems/state_manager.js
+++ systems/state_manager.js
@@ -4,6 +4,7 @@ import { zipParse, zipCreate } from '../imports/good_js.js'
 import { makeRuntime, runCode } from "../tools/js_runtime.js"
 import { dump, load } from "../imports/js_yaml.js"
 import { toKebabCase } from "../imports/good_js.js"
+import { normalizePath } from "../imports/good_js.js"
 const yaml = { stringify: dump, parse: load }
 
 // NOTE: despite mentioning a theme, this whole file should be completely usable without a browser/DOM
@@ -73,6 +74,7 @@ export class StateManager {
         this._config = config || {}
         this._theme = {...theme,...defaultTheme}
         this._fileSystemData = fileSystemData || {}
+        
 
         this._initStackFrame = new Error().stack.split("\n").slice(2).join("\n").trim()
         this.runtime = makeRuntime()
@@ -82,6 +84,63 @@ export class StateManager {
         this.prevState = structuredClone(this.activeState)
         this.loadStateCallbacks = new Set([...loadStateCallbacks])
         this.stateChangeCallbacks = new Set([...stateChangeCallbacks])
+        this.fileSystemEvents = {
+            get: new Event(),
+            write: new Event(),
+            rename: new Event(),
+            remove: new Event(),
+            // TODO: event on new directory, detect new directory based on file paths
+            // TODO: check if there are conflicting paths ("/a/b" is file and "/a/" is file)
+        }
+        this.fileSystem = {
+            getAllMetadata: ()=>{
+                return Object.fromEntries(Object.entries(this._fileSystemData).map(([path, data])=>{
+                    const metadata = {
+                        ...data,
+                    }
+                    delete metadata.content
+                    return [path, metadata]
+                }))
+            },
+            get: (path)=>{
+                if (typeof path !== "string" || path.length === 0) {
+                    throw new TypeError(`When calling fileSystem.rename, path must be a string\nEx: fileSystem.rename({path:"/path/to/file", path:"/new/path"})`)
+                }
+                path = normalizePath(path)
+                trigger(this.fileSystemEvents.get, {path,})
+                if (this._fileSystemData[path]) {
+                    return Object.freeze({...this._fileSystemData[path]})
+                }
+            },
+            write: ({path, content, ...other})=>{
+                if (typeof path !== "string" || path.length === 0) {
+                    throw new TypeError(`When calling fileSystem.rename, path must be a string\nEx: fileSystem.rename({path:"/path/to/file", path:"/new/path"})`)
+                }
+                path = normalizePath(path)
+                trigger(this.fileSystemEvents.write, {path, content, ...other})
+                this._fileSystemData[path] = {content, ...other}
+            },
+            rename: ({path, newPath,})=>{
+                if (typeof newPath !== "string" || newPath.length === 0) {
+                    throw new TypeError(`When calling fileSystem.rename, newPath must be a string\nEx: fileSystem.rename({path:"/path/to/file", newPath:"/new/path"})`)
+                }
+                if (typeof path !== "string" || path.length === 0) {
+                    throw new TypeError(`When calling fileSystem.rename, path must be a string\nEx: fileSystem.rename({path:"/path/to/file", path:"/new/path"})`)
+                }
+                path = normalizePath(path)
+                newPath = normalizePath(newPath)
+                trigger(this.fileSystemEvents.rename, {path, newPath})
+                this._fileSystemData[newPath] = this._fileSystemData[path]
+                delete this._fileSystemData[path]
+            },
+            remove: (path)=>{
+                if (typeof path !== "string" || path.length === 0) {
+                    throw new TypeError(`When calling fileSystem.rename, path must be a string\nEx: fileSystem.rename({path:"/path/to/file", path:"/new/path"})`)
+                }
+                path = normalizePath(path)
+                return delete this._fileSystemData[path]
+            },
+        }
     }
     
     // 
@@ -163,9 +222,6 @@ export class StateManager {
         )
         this._theme = merge({ oldData: this._theme, newData: value})
     }
-
-    get fileSystemData() { return Object.freeze(structuredClone(this._fileSystemData))  }
-    // set fileSystemData(value) {
     
     // 
     // serializing
